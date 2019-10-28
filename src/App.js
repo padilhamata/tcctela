ae3ac472f3b6d592014761110894cf8748651e0a
Lucas <lucaspadilhamata@hotmail.com>
2019-10-28 18:22:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import api from "./services/api";
+import "./App.css";
 
 function App() {
+  const [nome, setNome] = useState("");
+  const [quantidade, setquantidade] = useState("");
+  const [valor, setvalor] = useState("");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await api.post("/produto", { nome, quantidade, valor });
+    console.log(response);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <p> Crud Básico </p>
+        <label>Nome: </label>
+        <input
+          type="text"
+          name="nome"
+          id="nome"
+          onChange={event => setNome(event.target.value)}
+          value={nome}
+        />
+        <input
+          type="text"
+          name="quantidade"
+          id="quantidade"
+          onChange={event => setquantidade(event.target.value)}
+          value={quantidade}
+        />
+        <input
+          type="text"
+          name="valor"
+          id="valor"
+          onChange={event => setvalor(event.target.value)}
+          value={valor}
+        />
+
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
